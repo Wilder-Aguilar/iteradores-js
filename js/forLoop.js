846288dd3dd371bcd6201edf190b3a523f713def
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-06 22:04:49 +0200

    whileLoop

--- js/forLoop.js
+++ js/forLoop.js
@@ -93,4 +93,3 @@ for (i = 1; i < 100; i++) {
         console.log(`${i} buzz`)
     }
 }
-
---
+++ js/whileLoop.js
@@ -0,0 +1,33 @@
+//Se ejecuta mientras una condicion sea verdadera
+// let i = 0; //Inicializa el while
+
+// while(i < 8){  //Condición del while
+// console.log(`${i}`)
+//     i++;    //Incremento del while
+// }
+
+//FIZBUZZ
+
+// let i = 1
+// while(i < 50){  //Condición del while
+//     if (i % 3 === 0 & i%5 === 0) {
+//         console.log(`${i} FIZZBUZZ`)
+//     } else if (i % 3 === 0) {
+//         console.log(`${i} fizz`)
+//     } else if (i % 5 === 0) {
+//         console.log(`${i} buzz`)
+//     }
+//     i++;    //Incremento del while
+// }
+
+//DETECTAR PARES E IMPARES
+
+let i=1
+while(i<50){
+    if(i%2 === 0){
+        console.log (`${i} es par`)
+    } else{
+        console.log (`${i} es impar`)
+    }
+    i++;
+}
