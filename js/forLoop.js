a5e4b5486006ac70989343b28c95ab563ee5f284
Wilder-Aguilar <aguilarwilder@gmail.com>
2024-10-06 21:42:37 +0200

    forLoop-2

--- js/forLoop.js
+++ js/forLoop.js
@@ -33,3 +33,39 @@ const compras = [
 for( let i = 0; i < compras.length; i++ ){
     console.log(compras[i].nombre)
 }
+
+//BREAK AND CONTINUE
+    //Crear un forLoop que al detectar el numero "8" detenga su ejecución
+for(let i = 0; i<=20 ; i++){
+    if(i===8){
+        console.log("este es el número 8");
+        break;
+    } 
+    console.log(`Este es el número:${i}`)
+}
+    //Crear un forLoop que al detectar el numero "8" detenga su ejecución y lo nombre con letras
+    for(let i = 0; i<=20 ; i++){
+        if(i===8){
+            console.log("OCHO");
+            continue;
+        } 
+        console.log(`Este es el número:${i}`)
+    }
+
+//Ejemplo: lista de productos destacando al que tiene descuento
+const carrito = [
+    {nombre:"Monitor", precio: 1000},
+    {nombre:"TV", precio: 1800},
+    {nombre:"Celular", precio: 1500, descuento: true},
+    {nombre:"Tablet", precio: 1800, descuento: true},
+    {nombre:"Teclado", precio: 200, descuento: false},
+    {nombre:"Mouse", precio: 100},
+]
+
+for(let i = 0; i<carrito.length; i++){
+   if(carrito[i].descuento){
+    console.log(`El articulo ${carrito[i].nombre} tiene descuento`);
+    continue;
+    };
+    console.log(carrito[i].nombre)
+}
